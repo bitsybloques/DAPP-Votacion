f7dd1297d60f96f199dd1d46e67b84f04b6a5de0
bitsybloques <bitsybloques@gmail.com>
2018-03-21 10:16:52 +0100

    Projecte html acabat

--- contract.sol
+++ contract.sol
@@ -3,22 +3,30 @@ pragma solidity ^0.4.18;
 contract Voting {
 
   mapping (bytes32 => uint8) public votesReceived;
-  string[] public candidateList;
+  bytes32[] public candidateList;
 
-  function Voting() public {
-    candidateList = ["Mariona", "Anna"] ;
+  function Voting(bytes32[] candidateNames) public {
+    candidateList = candidateNames;
   }
 
   function totalVotesFor(bytes32 candidate) view public returns (uint8) {
-    
+    require(validCandidate(candidate));
     return votesReceived[candidate];
   }
 
   function voteForCandidate(bytes32 candidate) public {
-    
+    require(validCandidate(candidate));
     votesReceived[candidate]  += 1;
   }
 
 
+  function validCandidate(bytes32 candidate) view public returns (bool) {
+    for(uint i = 0; i < candidateList.length; i++) {
+      if (candidateList[i] == candidate) {
+        return true;
+      }
+    }
+    return false;
+   }
 }
 
